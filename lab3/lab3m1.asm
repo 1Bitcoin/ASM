dcf4c877c65882c9b6219e10738fbab32adf9944
zhigalkin <shigalkin@mail.ru>
2020-03-23 13:52:37 +0300

    lite fix

--- lab3/lab3m1.asm
+++ lab3/lab3m1.asm
@@ -1,5 +1,5 @@
 EXTRN X: byte
-EXTRN exit: far
+EXTRN work: far
 
 STK SEGMENT para STACK 'STACK'
         db 100 dup(0)
@@ -8,11 +8,12 @@ STK ENDS
 SC1 SEGMENT para public 'CODE'
         assume CS:SC1
 main:
-        mov AH, 8 ;читаем символ с клавиатуры
-        int 21h
+    mov AH, 8 ;читаем символ с клавиатуры
+    int 21h
 
-        mov X, AL
+    mov X, AL
 
-	jmp exit
+	call work
+
 SC1 ENDS
 END MAIN
--- lab3/lab3m2.asm
+++ lab3/lab3m2.asm
@@ -1,20 +1,21 @@
 PUBLIC X
-PUBLIC exit
+PUBLIC work
 
 SD1 SEGMENT para 'DATA'
-	X db 'X'
+	X db '0'
 SD1 ENDS
 
 SC1 SEGMENT para public 'CODE'
 	assume CS:SC1, DS:SD1
-exit:
-        sub X, 32 ; вычитаем 32 из кода символа
-        mov DL, X ; помещаем в DL X, для вывода буквы
+work:
+    sub X, 32 ; вычитаем 32 из кода символа
+    mov DL, X ; помещаем в DL X, для вывода буквы
 
-        mov AH, 2 ; вывод заглавного варианта буквы
-        int 21h
+    mov AH, 2 ; вывод заглавного варианта буквы
+    int 21h
 
-        mov AX, 4c00h ; конец программы
-        int 21h
+    mov AX, 4c00h ; конец программы
+    int 21h
+
 SC1 ENDS
-END
+END WORK
